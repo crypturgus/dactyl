36f54042cdeb51474298264e23e0b7229dba1677
Lukasz Siczek <lukaszsiczek1985@gmail.com>
2023-10-31 19:49:17 +0100

    Intial cleanup with some material UI elements and GlobalStyles introduction

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,27 +1,31 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+import { Dashboard } from "./pages/Dashboard";
+import { SignIn } from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <Wrapper>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/sign-in"} element={<SignIn />} />
+          <Route path={"/sign-up"} element={<SignUp />} />
+          <Route path={"/dashboard"} element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+  gap: 20px;
+`;
 
 export default App;
