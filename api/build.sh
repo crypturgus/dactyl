5c352e94af799b04b1a8513bf72886cd3547128b
Mateusz Kukawski <mkukawski@its.jnj.com>
2023-10-31 16:16:21 +0100

    d0 ???

--- api/build.sh
+++ api/build.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 set +x
 
-pip install -r requirements.txt
-python3 ./manage.py migrate
+
+# 2. Apply Django migrations
+echo "Apply database migrations"
+python manage.py migrate
+
+# 3. Create Django superuser
+echo "Create Django superuser"
+echo "
+import os
+from django.contrib.auth import get_user_model
+from django.db.utils import IntegrityError
+
+User = get_user_model()
+
+username = os.environ['DJANGO_SUPERUSER_USERNAME']
+email = os.environ['DJANGO_SUPERUSER_EMAIL']
+password = os.environ['DJANGO_SUPERUSER_PASSWORD']
+
+try:
+    User.objects.create_superuser(email, password)
+    print('Superuser created.')
+except IntegrityError:
+    print('Superuser already exists.')
+" | python manage.py shell
