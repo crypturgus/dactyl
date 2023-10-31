7149c9c8059dde33608dff86f732ae0107e2bc50
Mateusz Kukawski <mkukawski@its.jnj.com>
2023-10-31 17:01:07 +0100

    d0 ???

--- api/vercel_migrate.sh
+++ api/vercel_migrate.sh
@@ -7,24 +7,24 @@ poetry install
 
 # 2. Apply Django migrations
 echo "Apply database migrations"
-python manage.py migrate
-
-# 3. Create Django superuser
-echo "Create Django superuser"
-echo "
-import os
-from django.contrib.auth import get_user_model
-from django.db.utils import IntegrityError
-
-User = get_user_model()
-
-username = os.environ['DJANGO_SUPERUSER_USERNAME']
-email = os.environ['DJANGO_SUPERUSER_EMAIL']
-password = os.environ['DJANGO_SUPERUSER_PASSWORD']
-
-try:
-    User.objects.create_superuser(email, password)
-    print('Superuser created.')
-except IntegrityError:
-    print('Superuser already exists.')
-" | python manage.py shell
+python3 manage.py migrate
+#
+## 3. Create Django superuser
+#echo "Create Django superuser"
+#echo "
+#import os
+#from django.contrib.auth import get_user_model
+#from django.db.utils import IntegrityError
+#
+#User = get_user_model()
+#
+#username = os.environ['DJANGO_SUPERUSER_USERNAME']
+#email = os.environ['DJANGO_SUPERUSER_EMAIL']
+#password = os.environ['DJANGO_SUPERUSER_PASSWORD']
+#
+#try:
+#    User.objects.create_superuser(email, password)
+#    print('Superuser created.')
+#except IntegrityError:
+#    print('Superuser already exists.')
+#" | python manage.py shell
