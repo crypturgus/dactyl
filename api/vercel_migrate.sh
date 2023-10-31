48b6c23893772e61d5ed3fd58970343f21400921
Mateusz Kukawski <mkukawski@its.jnj.com>
2023-10-31 18:09:22 +0100

    d0 ???

--- api/vercel_migrate.sh
+++ api/vercel_migrate.sh
@@ -14,21 +14,21 @@ echo "Apply database migrations"
 python3 manage.py migrate
 #
 ## 3. Create Django superuser
-#echo "Create Django superuser"
-#echo "
-#import os
-#from django.contrib.auth import get_user_model
-#from django.db.utils import IntegrityError
-#
-#User = get_user_model()
-#
-#username = os.environ['DJANGO_SUPERUSER_USERNAME']
-#email = os.environ['DJANGO_SUPERUSER_EMAIL']
-#password = os.environ['DJANGO_SUPERUSER_PASSWORD']
-#
-#try:
-#    User.objects.create_superuser(email, password)
-#    print('Superuser created.')
-#except IntegrityError:
-#    print('Superuser already exists.')
-#" | python manage.py shell
+echo "Create Django superuser"
+echo "
+import os
+from django.contrib.auth import get_user_model
+from django.db.utils import IntegrityError
+
+User = get_user_model()
+
+username = os.environ['DJANGO_SUPERUSER_USERNAME']
+email = os.environ['DJANGO_SUPERUSER_EMAIL']
+password = os.environ['DJANGO_SUPERUSER_PASSWORD']
+
+try:
+    User.objects.create_superuser(email, password)
+    print('Superuser created.')
+except IntegrityError:
+    print('Superuser already exists.')
+" | python manage.py shell
