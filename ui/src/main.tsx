f902038ce8f1b3832e1534109d5a9bd2ac860fc7
Lukasz Siczek <lukaszsiczek1985@gmail.com>
2023-11-01 18:51:25 +0100

    Introduce apollo client with first call from FE, unblock CORS on BE

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import App from "./App.tsx";
 import GlobalStyle from "./GlobalStyles";
 
+const client = new ApolloClient({
+  uri: "http://0.0.0.0:8085/",
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <ApolloProvider client={client}>
+      <GlobalStyle />
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
