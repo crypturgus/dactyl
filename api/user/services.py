e4a5404b13f42dc14a2967402409e42f8ea6205e
Mateusz Kukawski <mkukawski@its.jnj.com>
2023-11-04 10:51:08 +0100

    d1 cleaup

--- api/user/services.py
+++ api/user/services.py
@@ -1,8 +1,6 @@
-from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 
-from services.notification_service import NotificationService
 from user.strawberry_models import RegisterUserInput, RegisterUserPayload
 
 UserModel = get_user_model()
@@ -19,7 +17,5 @@ def register_user_service(input: RegisterUserInput) -> RegisterUserPayload:
         )
     except ValidationError as e:
         errors.extend(e.messages)
-    else:
-        if settings.SEND_NOTIFICATIONS:
-            NotificationService().send_register_notification(user)
+
     return RegisterUserPayload(user=user, errors=errors)
