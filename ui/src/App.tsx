daf09d6601fa97227828d6b6dd073492433ccec5
Lukasz Siczek <lukaszsiczek1985@gmail.com>
2023-11-17 19:19:43 +0100

    Extract reusable layout and use it on pages

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,10 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import styled from "styled-components";
 import { PrivateRoute } from "./components/PrivateRoute";
 import { Dashboard } from "./pages/Dashboard";
+import { Invoice } from "./pages/Invoice";
 import { SignIn } from "./pages/SignIn";
 import { SignUp } from "./pages/SignUp";
 import { Welcome } from "./pages/Welcome";
-import { DASHBOARD_PATH, ROOT_PATH, SIGN_IN_PATH, SIGN_UP_PATH } from "./constants";
+import { DASHBOARD_PATH, INVOICE_PATH, ROOT_PATH, SIGN_IN_PATH, SIGN_UP_PATH } from "./constants";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path={ROOT_PATH} element={<PrivateRoute />}>
             <Route index element={<Welcome />} />
             <Route path={DASHBOARD_PATH} element={<Dashboard />} />
+            <Route path={INVOICE_PATH} element={<Invoice />} />
           </Route>
           <Route path={SIGN_IN_PATH} element={<SignIn />} />
           <Route path={SIGN_UP_PATH} element={<SignUp />} />
