5649e96245b5a81bffe23bfdf6cc4c0fb8a3e063
Lukasz Siczek <lukaszsiczek1985@gmail.com>
2023-11-21 13:40:31 +0100

    Move apps to the end in INSTALLED_APPS

--- api/app/settings.py
+++ api/app/settings.py
@@ -40,10 +40,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "user",
-    "company",
     "django_extensions",
     "corsheaders",
+    "user",
+    "company",
 ]
 
 MIDDLEWARE = [
